dd22cfcecaf3197cf63a146e6a5a574e33f0648a
BSenichak <bsenk77@gmail.com>
2025-05-03 17:52:20 +0300

    feat: add user registration and login functionality
    - Added bcrypt for password hashing in user registration.
    - Implemented a new API endpoint for user registration that hashes passwords and checks for existing users.
    - Created a login page (login.html) with a form for user login.
    - Updated the server to handle login requests and serve the new login page.
    - Modified the registration form to redirect to the login page upon successful registration.

--- server.js
+++ server.js
@@ -1,70 +1,93 @@
-require("dotenv").config()
-let db = require("./db")
-let http = require("http")
-let path = require("path")
-let fs = require("fs")
-let { Server } = require("socket.io")
+require("dotenv").config();
+let db = require("./db");
+let http = require("http");
+let path = require("path");
+let fs = require("fs");
+let { Server } = require("socket.io");
+let bcrypt = require("bcrypt")
 
-let pathToIndex = path.join(__dirname, "static", "index.html")
-let index = fs.readFileSync(pathToIndex, "utf-8")
+let pathToIndex = path.join(__dirname, "static", "index.html");
+let index = fs.readFileSync(pathToIndex, "utf-8");
 
-let pathToStyle = path.join(__dirname, "static", "style.css")
-let style = fs.readFileSync(pathToStyle, "utf-8")
+let pathToStyle = path.join(__dirname, "static", "style.css");
+let style = fs.readFileSync(pathToStyle, "utf-8");
 
-let pathToScript = path.join(__dirname, "static", "script.js")
-let script = fs.readFileSync(pathToScript, "utf-8")
+let pathToScript = path.join(__dirname, "static", "script.js");
+let script = fs.readFileSync(pathToScript, "utf-8");
 
-let pathToRegister = path.join(__dirname, "static", "register.html")
-let register = fs.readFileSync(pathToRegister, "utf-8")
+let pathToRegister = path.join(__dirname, "static", "register.html");
+let register = fs.readFileSync(pathToRegister, "utf-8");
 
-let pathToAuth = path.join(__dirname, "static", "auth.js")
-let auth = fs.readFileSync(pathToAuth, "utf-8")
+let pathToLogin = path.join(__dirname, "static", "login.html");
+let loginPage = fs.readFileSync(pathToLogin, "utf-8");
 
-let ser = http.createServer((req, res)=>{
-    switch(req.url){
-        case "/":
-            res.writeHead(200, {"content-type": "text/html"})
-            res.end(index)
-            break;
-        case "/register":
-            res.writeHead(200, {"content-type": "text/html"})
-            res.end(register)
-            break;
-        case "/style.css":
-            res.writeHead(200, {"content-type": "text/css"})
-            res.end(style)
-            break;
-        case "/script.js":
-            res.writeHead(200, {"content-type": "text/js"})
-            res.end(script)
-            break;
-        case "/auth.js":
-            res.writeHead(200, {"content-type": "text/js"})
-            res.end(auth)
-            break;
-        default:
-            res.writeHead(404, {"content-type": "text/html"})
-            res.end("<h1>404 not found</h1>")
-    }
-}).listen(3000, ()=>console.log("Server is on!"))
+let pathToAuth = path.join(__dirname, "static", "auth.js");
+let auth = fs.readFileSync(pathToAuth, "utf-8");
+
+let ser = http
+    .createServer((req, res) => {
+        switch (req.url) {
+            case "/":
+                res.writeHead(200, { "content-type": "text/html" });
+                res.end(index);
+                break;
+            case "/register":
+                res.writeHead(200, { "content-type": "text/html" });
+                res.end(register);
+                break;
+            case "/login":
+                res.writeHead(200, { "content-type": "text/html" });
+                res.end(loginPage);
+                break;
+            case "/style.css":
+                res.writeHead(200, { "content-type": "text/css" });
+                res.end(style);
+                break;
+            case "/script.js":
+                res.writeHead(200, { "content-type": "text/js" });
+                res.end(script);
+                break;
+            case "/auth.js":
+                res.writeHead(200, { "content-type": "text/js" });
+                res.end(auth);
+                break;
+            case "/api/register":
+                let data = "";
+                req.on("data", (chunk) => (data += chunk));
+                req.on("end", async () => {
+                    data = JSON.parse(data)
+                    console.log(data)
+                    let hash = await bcrypt.hash(data.password, 10)
+                    console.log(hash)
+                    console.log( await bcrypt.compare(data.password, hash))
+                    if(await db.checkExists(data.login)){
+                        res.end("User exist")
+                        return
+                    }
+                    await db.addUser(data.login, hash)
+                    
+                    res.end(JSON.stringify({link: '/login'}));
+                });
+                break;
+            default:
+                res.writeHead(404, { "content-type": "text/html" });
+                res.end("<h1>404 not found</h1>");
+        }
+    })
+    .listen(3000, () => console.log("Server is on!"));
 
 let io = new Server(ser);
 
-
-
-io.on("connection",async function(s){
-    console.log(s.id)
-    let messages = await db.getMessages()
-    messages = messages.map(m=>({name: m.login, text: m.content}))
-    io.emit("update", JSON.stringify(messages))
-    s.on("message",async (data)=>{
-        data = JSON.parse(data)
-        await db.addMessage(data.text, 2)
+io.on("connection", async function (s) {
+    console.log(s.id);
+    let messages = await db.getMessages();
+    messages = messages.map((m) => ({ name: m.login, text: m.content }));
+    io.emit("update", JSON.stringify(messages));
+    s.on("message", async (data) => {
+        data = JSON.parse(data);
+        await db.addMessage(data.text, 2);
         let messages = await db.getMessages();
-        messages = messages.map(m=>({name: m.login, text: m.content}))
-        io.emit("update", JSON.stringify(messages))
-    })
-})
-
-
-
+        messages = messages.map((m) => ({ name: m.login, text: m.content }));
+        io.emit("update", JSON.stringify(messages));
+    });
+});
